c0828b9d9a6aa468a97f035db9c05fe7ac0368c9
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 14:58:48 +0530

    Update vendorAuthRoutes.js

--- routes/vendorAuthRoutes.js
+++ routes/vendorAuthRoutes.js
@@ -1,1 +1,10 @@
+import express from "express";
+import { registerVendor, loginVendor } from "../controllers/vendorAuthController.js";
+
+const router = express.Router();
+
+router.post("/register", registerVendor);
+router.post("/login", loginVendor);
+
+export default router;
 
