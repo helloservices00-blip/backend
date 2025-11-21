3e18a2f4d8cb6f80a8af68bd56643e5c0722e77c
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 14:41:29 +0530

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,1 +1,13 @@
+import express from "express";
+import adminAuth from "../middleware/adminAuth.js";
+import { getCategories, addCategory, updateCategory, deleteCategory } from "../controllers/categoryController.js";
+
+const router = express.Router();
+
+router.get("/", getCategories);
+router.post("/", adminAuth, addCategory);
+router.put("/:id", adminAuth, updateCategory);
+router.delete("/:id", adminAuth, deleteCategory);
+
+export default router;
 
