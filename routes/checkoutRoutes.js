b6a9df73b70332fadab33a6019861fc038bc0aa3
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 14:51:46 +0530

    Update checkoutRoutes.js

--- routes/checkoutRoutes.js
+++ routes/checkoutRoutes.js
@@ -1,1 +1,10 @@
+import express from "express";
+import auth from "../middleware/auth.js";
+import { createCheckoutSession } from "../controllers/checkoutController.js";
+
+const router = express.Router();
+
+router.post("/create-session", auth, createCheckoutSession);
+
+export default router;
 
