43f23662f231c5ee87f0907896dc0d8f0d1a3e05
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 17:01:58 +0530

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,1 +1,14 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  products: [{
+    product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+    quantity: { type: Number, default: 1 }
+  }],
+  totalPrice: { type: Number },
+  status: { type: String, default: "pending" },
+}, { timestamps: true });
+
+export default mongoose.model("Order", OrderSchema);
 
