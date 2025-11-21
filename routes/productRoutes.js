cc13813a7bd5e3da40b14e9dc8ed4fa9798555c0
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 15:07:23 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,1 +1,11 @@
+import express from "express";
+import { getProducts, getProductById } from "../controllers/productController.js";
+
+const router = express.Router();
+
+// List products with query params
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+
+export default router;
 
