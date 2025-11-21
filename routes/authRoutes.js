7562b79e4663d0345dc98e2632140d9dcf12483d
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 17:35:02 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import { auth } from "../middleware/auth.js";
+import { login, register } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/register", register);
 router.post("/login", login);
+router.post("/register", register);
+router.get("/profile", auth, (req, res) => {
+  res.json(req.user);
+});
 
 export default router;
