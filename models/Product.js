38bb48ffbd3c1c54bb21efd9e12b6ccf52f82183
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 15:29:33 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,40 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+
+    vendorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendor",
+      required: true,
+    },
+
+    moduleId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Module",
+      required: true,
+    },
+
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+
+    subcategoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Subcategory",
+      required: true,
+    },
+
+    images: { type: [String], default: [] },
+    stock: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
 
