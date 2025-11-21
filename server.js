f66b89265665e2552e9b9718e12e1ef9508fef40
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 11:52:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,27 +16,29 @@ import subcategoryRoutes from "./routes/subcategoryRoutes.js";
 dotenv.config();
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// MAIN ROUTES
+// Main Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/vendors", vendorRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
-// NEW MODULE–CATEGORY–SUBCATEGORY ROUTES
+// Module → Category → Subcategory
 app.use("/api/modules", moduleRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/subcategories", subcategoryRoutes);
 
-// DATABASE & SERVER
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log("MongoDB connected");
-  app.listen(process.env.PORT || 5000, () =>
-    console.log("Server running")
-  );
-})
-.catch(err => console.log(err));
+// Database and server
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Server running");
+    });
+  })
+  .catch((err) => console.log(err));
