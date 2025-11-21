0ddcc6427aefc8d4c69bda721dbc7a77fe60aaa3
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 14:39:05 +0530

    Update moduleController.js

--- controllers/moduleController.js
+++ controllers/moduleController.js
@@ -1,1 +1,22 @@
+import Module from "../models/Module.js";
+
+export const getModules = async (req, res) => {
+  const modules = await Module.find();
+  res.json(modules);
+};
+
+export const addModule = async (req, res) => {
+  const module = await Module.create(req.body);
+  res.json(module);
+};
+
+export const updateModule = async (req, res) => {
+  const module = await Module.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(module);
+};
+
+export const deleteModule = async (req, res) => {
+  await Module.findByIdAndDelete(req.params.id);
+  res.json({ message: "Module deleted" });
+};
 
