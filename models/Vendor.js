eab6fcfef21021a34c6e7f87f31ff5772345f1ad
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 15:00:05 +0530

    Update Vendor.js

--- models/Vendor.js
+++ models/Vendor.js
@@ -1,1 +1,24 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const vendorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+// Hash password before save
+vendorSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare password
+vendorSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+export default mongoose.model("Vendor", vendorSchema);
 
