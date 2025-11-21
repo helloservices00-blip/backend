b19952ea1233782b54a3b8964685256a4941d80a
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 17:19:37 +0530

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { placeOrder, getUserOrders } from "../controllers/orderController.js";
-import auth from "../middleware/auth.js";
+import { auth } from "../middleware/auth.js";
+import { adminAuth } from "../middleware/adminAuth.js";
+import { createOrder, getUserOrders, getAllOrders, updateOrderStatus } from "../controllers/orderController.js";
 
 const router = express.Router();
 
-router.post("/", auth, placeOrder); // place order
-router.get("/", auth, getUserOrders); // get user orders
+// User routes
+router.post("/", auth, createOrder);
+router.get("/my-orders", auth, getUserOrders);
+
+// Admin routes
+router.get("/", adminAuth, getAllOrders);
+router.put("/:id/status", adminAuth, updateOrderStatus);
 
 export default router;
-
