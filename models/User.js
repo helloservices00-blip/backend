a85104aec6cc8cd43baaf406b623b99f430d63bd
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 16:58:28 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,24 +1,21 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+  isAdmin: { type: Boolean, default: false },
+}, { timestamps: true });
 
-// Hash password before save
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+UserSchema.pre("save", async function(next) {
+  if(!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-// Compare password
-userSchema.methods.comparePassword = function (password) {
+UserSchema.methods.comparePassword = function(password){
   return bcrypt.compare(password, this.password);
-};
+}
 
-export default mongoose.model("User", userSchema);
-
+export default mongoose.model("User", UserSchema);
