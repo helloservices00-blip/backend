879eac686cb92dfdbf88c38d88260a393866c5dc
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 14:55:16 +0530

    Update vendorProductRoutes.js

--- routes/vendorProductRoutes.js
+++ routes/vendorProductRoutes.js
@@ -1,1 +1,13 @@
+import express from "express";
+import vendorAuth from "../middleware/vendorAuth.js";
+import { getVendorProducts, addProduct, updateProduct, deleteProduct } from "../controllers/vendorProductController.js";
+
+const router = express.Router();
+
+router.get("/", vendorAuth, getVendorProducts); // list vendor products
+router.post("/", vendorAuth, addProduct); // add product
+router.put("/:id", vendorAuth, updateProduct); // update product
+router.delete("/:id", vendorAuth, deleteProduct); // delete product
+
+export default router;
 
