7f3c5867c4a0dab7efb4c2ecc355a21740825079
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 13:35:15 +0530

    Delete routes directory

--- routes/moduleRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createModule, getModules } from "../controllers/moduleController.js";
-
-const router = express.Router();
-
-router.post("/", createModule);
-router.get("/", getModules);
-
-export default router;
