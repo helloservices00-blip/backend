dcdfdf6ef9ba051a68dfacab1e09c752a02b9008
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 17:00:37 +0530

    Update Subcategory.js

--- models/Subcategory.js
+++ models/Subcategory.js
@@ -1,26 +1,8 @@
 import mongoose from "mongoose";
 
-const subcategorySchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    categoryId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Category",
-      required: true,
-    },
-    moduleId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Module",
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
+const SubcategorySchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" }
+}, { timestamps: true });
 
-export default mongoose.model("Subcategory", subcategorySchema);
-
+export default mongoose.model("Subcategory", SubcategorySchema);
