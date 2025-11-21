13ba56319be2449f7deaee01fc7e5bb8a1eef706
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 14:37:27 +0530

    Update moduleRoutes.js

--- routes/moduleRoutes.js
+++ routes/moduleRoutes.js
@@ -1,1 +1,13 @@
+import express from "express";
+import adminAuth from "../middleware/adminAuth.js";
+import { getModules, addModule, updateModule, deleteModule } from "../controllers/moduleController.js";
+
+const router = express.Router();
+
+router.get("/", getModules);
+router.post("/", adminAuth, addModule);
+router.put("/:id", adminAuth, updateModule);
+router.delete("/:id", adminAuth, deleteModule);
+
+export default router;
 
