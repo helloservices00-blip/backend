b48d2beacaa646dbbf758e8a0e69677eb8cd31b5
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 14:47:59 +0530

    Update subcategoryController.js

--- controllers/subcategoryController.js
+++ controllers/subcategoryController.js
@@ -1,1 +1,22 @@
+import Subcategory from "../models/Subcategory.js";
+
+export const getSubcategories = async (req, res) => {
+  const subcategories = await Subcategory.find();
+  res.json(subcategories);
+};
+
+export const addSubcategory = async (req, res) => {
+  const subcategory = await Subcategory.create(req.body);
+  res.json(subcategory);
+};
+
+export const updateSubcategory = async (req, res) => {
+  const subcategory = await Subcategory.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(subcategory);
+};
+
+export const deleteSubcategory = async (req, res) => {
+  await Subcategory.findByIdAndDelete(req.params.id);
+  res.json({ message: "Subcategory deleted" });
+};
 
