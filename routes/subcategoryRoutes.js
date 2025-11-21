83b003f3d67b9a7f73e989c87429928682cdd5a9
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 14:45:33 +0530

    Update subcategoryRoutes.js

--- routes/subcategoryRoutes.js
+++ routes/subcategoryRoutes.js
@@ -1,1 +1,13 @@
+import express from "express";
+import adminAuth from "../middleware/adminAuth.js";
+import { getSubcategories, addSubcategory, updateSubcategory, deleteSubcategory } from "../controllers/subcategoryController.js";
+
+const router = express.Router();
+
+router.get("/", getSubcategories);
+router.post("/", adminAuth, addSubcategory);
+router.put("/:id", adminAuth, updateSubcategory);
+router.delete("/:id", adminAuth, deleteSubcategory);
+
+export default router;
 
