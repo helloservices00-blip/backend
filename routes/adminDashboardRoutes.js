a71e136a74b7b7346157cf508bd54eb277954a46
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 17:24:26 +0530

    Update adminDashboardRoutes.js

--- routes/adminDashboardRoutes.js
+++ routes/adminDashboardRoutes.js
@@ -1,1 +1,10 @@
+import express from "express";
+import { adminAuth } from "../middleware/adminAuth.js";
+import { getDashboardStats } from "../controllers/adminDashboardController.js";
+
+const router = express.Router();
+
+router.get("/", adminAuth, getDashboardStats);
+
+export default router;
 
