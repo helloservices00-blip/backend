3a79ec45ef520098538a965acf1ddfa2a2025d76
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 17:00:09 +0530

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,16 +1,8 @@
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true, unique: true },
-    moduleId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Module",
-      required: true,
-    }
-  },
-  { timestamps: true }
-);
+const CategorySchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  module: { type: mongoose.Schema.Types.ObjectId, ref: "Module" }
+}, { timestamps: true });
 
-export default mongoose.model("Category", categorySchema);
-
+export default mongoose.model("Category", CategorySchema);
