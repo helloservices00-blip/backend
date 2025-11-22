981f8a7a910c4520506db9d9acd6ff427fdfabca
helloservices00-blip <helloservices00@gmail.com>
2025-11-22 10:42:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.use("/api/admin/dashboard", adminDashboardRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log("MongoDB connected");
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-})
-.catch(err => console.log(err));
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => console.log(err));
